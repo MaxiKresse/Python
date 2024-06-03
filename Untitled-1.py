871b06508fd6a039f7f89fd59bfb76d93df63396
MaxiKresse <169070284+MaxiKresse@users.noreply.github.com>
2024-06-03 11:55:46 -0400

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+Frase = input("Ingresa una palabra: ").lower().replace(" ","")
+
+def Palindromo():
+    alreves=Frase[::-1]
+    if alreves == Frase:
+        print(f"{Frase} es un palindromo")
+    else:
+        print(f"{Frase} no es un palindromo")
+Palindromo()                
